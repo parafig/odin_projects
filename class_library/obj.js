40fac07dcb871f5d4da87c6c5dfb42c2127495bb
parafig <parafigmaedicion@proton.me>
2022-09-02 23:35:28 -0400

    replace existing object function and methods with classes

--- class_library/obj.js
+++ class_library/obj.js
@@ -1,95 +1,99 @@
 
 
-function Book(title, author) {
+class Book {
+    constructor(title, author) {
     this.title = title;
     this.author = author;
     this.read = false;
+    }
 }
 
-function Library() {
-    this.myLibrary = [];
-}
-
-Library.prototype.addBookToLibrary = function() {
-    const newBookTitle = document.querySelector("#title")
-    const newBookAuthor = document.querySelector("#author")
-    const title = newBookTitle.value
-    const author = newBookAuthor.value
-    const newBook = new Book(title, author);
-    this.myLibrary.push(newBook);
-}
-
-Library.prototype.displayBooks = function () {
-    const bookTable = document.querySelector("tbody");
-
-    while (bookTable.firstChild) {
-        bookTable.firstChild.remove()
+class Library {
+    constructor() {
+        this.myLibrary = [];
     }
 
-    this.myLibrary.forEach((book) =>  {
-        const bookRow = document.createElement("tr");
+    addBookToLibrary() {
+        const newBookTitle = document.querySelector("#title")
+        const newBookAuthor = document.querySelector("#author")
+        const title = newBookTitle.value
+        const author = newBookAuthor.value
+        const newBook = new Book(title, author);
+        this.myLibrary.push(newBook);
+    }
 
-        const bookNumber = document.createElement("td");
-        bookNumber.textContent = this.myLibrary.indexOf(book) + 1;
+    displayBooks() {
+        const bookTable = document.querySelector("tbody");
 
-        const bookTitle = document.createElement("td");
-        bookTitle.textContent = book.title;
-
-        const bookAuthor = document.createElement("td");
-        bookAuthor.textContent = book.author;
-
-        const removeCell = document.createElement("td");
-        const removeBtn = document.createElement("button")
-        removeBtn.classList.add("remove-btn")
-        removeBtn.textContent = "remove"
-        const indexItem = this.myLibrary.indexOf(book);
-        removeBtn.dataset.num = indexItem
-        removeBtn.addEventListener("click", this.removeBook.bind(this, indexItem))
-        removeCell.appendChild(removeBtn);
-
-        const readCell = document.createElement("td")
-        const readBtn = document.createElement("button")
-        readBtn.dataset.num =  indexItem;
-        readBtn.classList.add("read-btn")
-        readBtn.addEventListener("click", this.readBook.bind(this, indexItem))
-
-        if (this.myLibrary[this.myLibrary.indexOf(book)].read) {
-            readBtn.textContent = "read"
-        } else {
-            readBtn.textContent = "Unread"
+        while (bookTable.firstChild) {
+            bookTable.firstChild.remove()
         }
 
-        readCell.appendChild(readBtn)
+        this.myLibrary.forEach((book) =>  {
+            const bookRow = document.createElement("tr");
 
-        bookRow.appendChild(bookNumber);
-        bookRow.appendChild(bookTitle);
-        bookRow.appendChild(bookAuthor);
-        bookRow.appendChild(removeCell);
-        bookRow.appendChild(readCell)
+            const bookNumber = document.createElement("td");
+            bookNumber.textContent = this.myLibrary.indexOf(book) + 1;
 
-        bookTable.appendChild(bookRow);
-    })
-}
+            const bookTitle = document.createElement("td");
+            bookTitle.textContent = book.title;
 
-Library.prototype.removeBook = function(indexItem) {
+            const bookAuthor = document.createElement("td");
+            bookAuthor.textContent = book.author;
+
+            const removeCell = document.createElement("td");
+            const removeBtn = document.createElement("button")
+            removeBtn.classList.add("remove-btn")
+            removeBtn.textContent = "remove"
+            const indexItem = this.myLibrary.indexOf(book);
+            removeBtn.dataset.num = indexItem
+            removeBtn.addEventListener("click", this.removeBook.bind(this, indexItem))
+            removeCell.appendChild(removeBtn);
+
+            const readCell = document.createElement("td")
+            const readBtn = document.createElement("button")
+            readBtn.dataset.num =  indexItem;
+            readBtn.classList.add("read-btn")
+            readBtn.addEventListener("click", this.readBook.bind(this, indexItem))
+
+            if (this.myLibrary[this.myLibrary.indexOf(book)].read) {
+                readBtn.textContent = "read"
+            } else {
+                readBtn.textContent = "Unread"
+            }
+
+            readCell.appendChild(readBtn)
+
+            bookRow.appendChild(bookNumber);
+            bookRow.appendChild(bookTitle);
+            bookRow.appendChild(bookAuthor);
+            bookRow.appendChild(removeCell);
+            bookRow.appendChild(readCell)
+
+            bookTable.appendChild(bookRow);
+        })
+    }
+
+    removeBook(indexItem) {
         this.myLibrary.splice(indexItem, 1)
         this.displayBooks();
-}
+    }
 
-Library.prototype.readBook = function(indexItem){
+    readBook(indexItem) {
         if (this.myLibrary[indexItem].read) {
             this.myLibrary[indexItem].read = false;
         } else {
             this.myLibrary[indexItem].read = true;
         }
         this.displayBooks();
-}
+    }
 
-Library.prototype.addDisplayBook = function() {
-    addBtn.addEventListener('click', () => {
-        this.addBookToLibrary()
-        this.displayBooks();
-    })
+    addDisplayBook() {
+        addBtn.addEventListener('click', () => {
+            this.addBookToLibrary()
+            this.displayBooks();
+        })
+    }
 }
 
 const newBtn = document.querySelector(".new-btn")
