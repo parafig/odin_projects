8ca0c44caa0e9636ba281826d8f6c3d926cd0ded
parafig <parafigmaedicion@proton.me>
2022-09-07 17:39:18 -0400

    refactor todo module, separate the code tasks in different functions

--- src/index.js
+++ src/index.js
@@ -4,45 +4,70 @@ const domManipulation = (function() {
     const container = document.createElement("div");
     container.classList.add("container");
 
-    const todoContent = document.createElement("div")
-    todoContent.classList.add("todo-content")
+    const todoContent = document.createElement("div");
+    todoContent.classList.add("todo-content");
 
-    const todoList = document.createElement("ul");
-    todoList.classList.add("todo-list")
+    const createTodoContent = () => {
+        console.log("New Content");
 
-    const btn = document.createElement("button");
-    btn.textContent = "add";
-    btn.classList.add("btn-ex");
+        const todoList = document.createElement("ul");
+        todoList.classList.add("todo-list");
+        todoContent.addEventListener('click', newTask);
 
-    todoContent.addEventListener("click", (e) => {
+        const addBtn = createAddBtn(); 
+
+        todoContent.appendChild(addBtn);
+        todoContent.appendChild(todoList)
+
+    }
+
+    const createAddBtn = () => {
+        const btn = document.createElement("button");
+        btn.textContent = "add";
+        btn.classList.add("btn-ex");
+        return btn;
+    }
+
+    const createInput = () => {
+        const inputUser = document.createElement("input");
+        inputUser.classList.add("msg-from-user");
+    }
+
+    const newTask = (e) => {
         if (e.target.tagName == "BUTTON") {
             const btn = document.querySelector(".btn-ex")
             todoContent.removeChild(btn);
+
             const inputUser = document.createElement("input");
             inputUser.classList.add("msg-from-user");
+
             todoContent.append(inputUser);
             inputUser.addEventListener("keypress", (e) => {
                 if (e.key == "Enter") {
-                    inputFromUser = document.querySelector(".msg-from-user");
-                    dom = document.querySelector(".todo-list")
+                    const taskInput = document.querySelector(".msg-from-user");
+
                     const todo = document.createElement("li");
-                    todo.textContent = inputFromUser.value;
-                    dom.appendChild(todo);
-                    todoContent.removeChild(inputFromUser)
+                    todo.textContent = taskInput.value;
+
+                    const todoList = document.querySelector(".todo-list")
+                    todoList.appendChild(todo);
+
+                    todoContent.removeChild(taskInput)
+
                     const btn = document.createElement("button");
                     btn.textContent = "add";
                     btn.classList.add("btn-ex");
                     todoContent.appendChild(btn);
-
                 }
             })
         }
-    })
+    }
+
 
     const body = document.querySelector("body");
-    todoContent.appendChild(btn);
-    todoContent.appendChild(todoList);
-    container.appendChild(todoContent)
-    body.appendChild(container)
+    createTodoContent();
+    container.appendChild(todoContent);
+    body.appendChild(container);
 
 })()
+
