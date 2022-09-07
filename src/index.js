6487c784ee1bb1396727bddf43934db9391d940f
parafig <parafigmaedicion@proton.me>
2022-09-07 17:08:59 -0400

    add raw event listener function to create new tasks in todo list

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const domManipulation = (function() {
     const container = document.createElement("div");
     container.classList.add("container");
 
+    const todoContent = document.createElement("div")
+    todoContent.classList.add("todo-content")
+
     const todoList = document.createElement("ul");
     todoList.classList.add("todo-list")
 
@@ -11,35 +14,35 @@ const domManipulation = (function() {
     btn.textContent = "add";
     btn.classList.add("btn-ex");
 
-    const inputUser = document.createElement("input");
-    inputUser.classList.add("msg-from-user");
+    todoContent.addEventListener("click", (e) => {
+        if (e.target.tagName == "BUTTON") {
+            const btn = document.querySelector(".btn-ex")
+            todoContent.removeChild(btn);
+            const inputUser = document.createElement("input");
+            inputUser.classList.add("msg-from-user");
+            todoContent.append(inputUser);
+            inputUser.addEventListener("keypress", (e) => {
+                if (e.key == "Enter") {
+                    inputFromUser = document.querySelector(".msg-from-user");
+                    dom = document.querySelector(".todo-list")
+                    const todo = document.createElement("li");
+                    todo.textContent = inputFromUser.value;
+                    dom.appendChild(todo);
+                    todoContent.removeChild(inputFromUser)
+                    const btn = document.createElement("button");
+                    btn.textContent = "add";
+                    btn.classList.add("btn-ex");
+                    todoContent.appendChild(btn);
 
-
-    const addToDom = (msg) => {
-        dom = document.querySelector(".todo-list")
-        const todo = document.createElement("li");
-        todo.textContent = msg;
-        dom.appendChild(todo);
-    }
-
-    btn.addEventListener("click", addToDom.bind(this, "new todo"));
-
-    inputUser.addEventListener("keypress", (e) => {
-        console.log(e)
-        if (e.key == "Enter") {
-            inputFromUser = document.querySelector(".msg-from-user");
-            dom = document.querySelector(".todo-list")
-            const todo = document.createElement("li");
-            todo.textContent = inputFromUser.value;
-            dom.appendChild(todo);
+                }
+            })
         }
     })
 
-
     const body = document.querySelector("body");
-    body.appendChild(btn);
-    container.appendChild(todoList);
-    container.append(inputUser)
+    todoContent.appendChild(btn);
+    todoContent.appendChild(todoList);
+    container.appendChild(todoContent)
     body.appendChild(container)
 
 })()
